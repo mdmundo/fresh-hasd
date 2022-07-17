5f886e5f6e4b74ca708ae5dec0dfe79aab820e1e
Edmundo Paulino <manyymoore@gmail.com>
2022-07-17 13:44:08 +0000

    design appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, createTheme, Paper, ThemeProvider, Typography } from "@mui/material";
+import { AppBar, Container, createTheme, Grid, Paper, ThemeProvider, Toolbar, Typography } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 
 const App = () => {
@@ -10,10 +10,31 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <AppBar position="sticky" enableColorOnDark>
+        <Toolbar>
+          <Grid container direction="row" justifyContent="space-between" alignItems="center">
+            <Grid item>
+              <Typography children="Novo Hinário Adventista" />
+            </Grid>
+            <Grid item>
+              <Grid container columnSpacing={2} direction="row" alignItems="center">
+                <Grid item>
+                  <Typography children="Github" />
+                </Grid>
+                <Grid item>
+                  <Typography children="Barra de Pesquisa" />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
       <Container maxWidth="md">
-        <Paper elevation={5} sx={{ p: 2, my: 3 }}>
-          <Typography variant="h1" children="Novo Hinário Adventista" />
-        </Paper>
+        {new Array(10).fill(0).map(() => (
+          <Paper elevation={5} sx={{ p: 2, my: 3 }}>
+            <Typography variant="h1" children="Novo Hinário Adventista" />
+          </Paper>
+        ))}
       </Container>
     </ThemeProvider>
   );
