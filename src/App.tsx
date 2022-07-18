b0d6a02dbefa5b86b993909cd7d6d3920c7e238a
Edmundo Paulino <manyymoore@gmail.com>
2022-07-18 12:25:31 +0000

    bar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {
   AppBar,
+  Box,
   Button,
   Card,
   CardActions,
@@ -7,12 +8,14 @@ import {
   Container,
   createTheme,
   Grid,
+  IconButton,
+  Paper,
   ThemeProvider,
   Toolbar,
   Typography,
 } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import { YouTube } from "@mui/icons-material";
+import { GitHub, YouTube } from "@mui/icons-material";
 import hymns from "./hymns.json";
 import { Logo } from "./Icons";
 
@@ -24,29 +27,54 @@ const theme = createTheme({
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <AppBar position="sticky">
-      <Toolbar>
-        <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ py: 2 }}>
-          <Grid item>
-            <Grid container columnSpacing={1.25} direction="row" alignItems="center" justifyContent="flex-start">
-              <Grid item>
-                <Logo fontSize="large" />
-              </Grid>
-              <Grid item>
-                <Typography variant="h6" children="Novo Hinário Adventista" />
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ py: 2 }}>
+            <Grid item>
+              <Grid container columnSpacing={1.25} direction="row" alignItems="center" justifyContent="flex-start">
+                <Grid item>
+                  <Logo fontSize="large" />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h6" children="Novo Hinário Adventista" />
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 2, mb: 10 }}>
+        <Grid container direction="row" justifyContent="center" alignItems="flex-start" spacing={2}>
+          {hymns.map((hymn) => (
+            <Hymn key={hymn.id} hymn={hymn} />
+          ))}
         </Grid>
-      </Toolbar>
-    </AppBar>
-    <Container sx={{ mt: 2, mb: 10 }}>
-      <Grid container direction="row" justifyContent="center" alignItems="flex-start" spacing={2}>
-        {hymns.map((hymn) => (
-          <Hymn key={hymn.id} hymn={hymn} />
-        ))}
-      </Grid>
-    </Container>
+      </Container>
+      <Paper
+        square
+        sx={{
+          p: 1.5,
+          mt: "auto",
+        }}
+      >
+        <Grid container direction="row" justifyContent="center" alignItems="center" columnSpacing={1}>
+          <Grid item children={<Typography align="center" variant="caption" children="por Edmundo Paulino" />} />
+          <Grid
+            item
+            children={
+              <IconButton
+                size="small"
+                color="inherit"
+                children={<GitHub />}
+                href="https://github.com/mdmundo/fresh-hasd"
+              />
+            }
+          />
+          <Grid item children={<Typography align="center" variant="caption" children="criado em 2022" />} />
+        </Grid>
+      </Paper>
+    </Box>
   </ThemeProvider>
 );
 
